b61027717f22c5de4d8872c0cd4f0d403eb06cdb
Raven Hedden <ravenhedden@google.com>
2024-09-20 16:31:55 -0400

    add support for both state and edu

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 8080;
 
 app.use(express.static('public'));
 
+// Serve index-edu.html for the edu path
+app.get('/state', (req, res) => {
+  res.sendFile(__dirname + '/public/index-state.html');
+});
+
+// Serve index-state.html for the state path
+app.get('/edu', (req, res) => {
+  res.sendFile(__dirname + '/public/index-edu.html');
+});
+
 app.listen(port, () => {
   console.log(`Solar web app listening on port ${port}`);
 });
